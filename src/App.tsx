6574c8b49b67b88ec439b83c0a52a45577c39ac9
Sallam Rady <sallamrady@gmail.com>
2024-06-03 21:13:35 +0300

    install nessecery packages and create lan context and set config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
+import getLangCookie from "./methods/cookies/getLangCookie";
+import axios from "axios";
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import ArTranslation from "./translate/ArTranslation";
+import EnTranslation from "./translate/EnTranslation";
+import { useContext, useMemo } from "react";
+import { LangContext } from "./context/LangContext";
+import "./App.css";
+import { Button } from "@mui/material";
+
+// TODO:: Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+// TODO::define current language
+const currentLang = getLangCookie();
+axios.defaults.headers.common["from"] = "website";
+axios.defaults.headers.common["lang"] = currentLang === "ar" ? "ar" : "en";
+// TODO::config react18inext
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: EnTranslation },
+    ar: { translation: ArTranslation },
+  },
+  lng: currentLang,
+  fallbackLng: "en",
+  interpolation: { escapeValue: false },
+});
 
 function App() {
-  return (
+  // declare and define component state and variables
+  const langContext = useContext(LangContext);
+  const lang = langContext.lang();
+  let { t } = useTranslation();
+
+  // TODO::APP Component
+  const AppComponent = () => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>{t("welcome")}</p>
+        <Button
+          variant="contained"
+          onClick={() => langContext.changeLang(lang == "ar" ? "en" : "ar")}
         >
-          Learn React
-        </a>
+          Change Language to {lang == "ar" ? "EN" : "AR"}
+        </Button>
       </header>
     </div>
   );
+
+  const render = useMemo(() => {
+    switch (lang) {
+      case "ar":
+        return (
+          <CacheProvider value={cacheRtl}>
+            <AppComponent />
+          </CacheProvider>
+        );
+      default:
+        return <AppComponent />;
+    }
+  }, [lang]);
+
+  // return component ui.
+  return render;
 }
 
 export default App;
